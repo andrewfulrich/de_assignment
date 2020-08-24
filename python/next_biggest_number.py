2878017782bd6e599e7a2dc62e9622a24a23004a
Andrew Ulrich <andrewfulrich@gmail.com>
2020-08-24 16:42:56 -0500

    go for simple

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
 import sys
+from itertools import permutations 
 
 def main():
     next_biggest_number(sys.argv[1])
 
 
 def next_biggest_number(num):
-    #TODO: Implement me!
-    return 0
+    digits = [int(d) for d in str(num)]
+    # return if it's just a single digit
+    if len(digits) == 1:
+        return -1
+
+    # return if it's already the biggest
+    sorted_reversed=digits[::-1]
+    sorted_reversed.sort(reverse=True)
+    if sorted_reversed == digits:
+        return -1
+
+    #simplicity or performance? for this case, I'll take simplicity
+    #get all permutations
+    perms=[''.join(p) for p in permutations(str(num))]
+    #filter for the ones that are greater than num
+    numbers=list(filter(lambda perm: perm>num, [int(perm) for perm in perms]))
+    #return the least one from that filtered set
+    numbers.sort()
+    return numbers[0]
 
 if __name__ == "__main__":
     main()
